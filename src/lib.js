2f68071480fee9e0faf7a02b60fb7a4423f56133
Raja Pothanaboiena <44020191+rajapothana@users.noreply.github.com>
2018-10-25 15:27:46 +0530

    now lib,js file is working for compose problem

--- src/lib.js
+++ src/lib.js
@@ -7,7 +7,7 @@ const makeConstant = function (source) {
 const makeCounterFromN = function (argv) {
   let count = argv;
   return function (){
-  return count++;
+    return count++;
   }
 }
 
@@ -30,16 +30,32 @@ const makeDeltaTracker = function (firstArgv) {
     return DeltaTracker;
   }
 }
+const lengthOf=function(collection) {return collection.length};
+const decrement=function(number){return number-1};
+const isNotZero=function(x) {return x!=0};
+const removeZeroes=function(array) {return array.filter(isNotZero)};
+const concatWith=function(arrayToConcatWith,anotherArray) {
+  return arrayToConcatWith.concat(anotherArray);
+};
 
-  /*const makeFiboGenerator = undefined;
+
+const compose = function (arg1,arg2) {
+  return function (source1,source2) {
+    return arg1(arg2(source1,source2));
+  }
+}
+
+
+
+
+/*const makeFiboGenerator = undefined;
 const makeCycler = undefined;
-const compose = undefined;
 */
-  exports.makeConstant=makeConstant;
-  exports.makeCounterFromN=makeCounterFromN;
-  exports.makeCounterFromZero=makeCounterFromZero;
-  exports.makeDeltaTracker=makeDeltaTracker;
-  //exports.curry=curry;
-  /*exports.makeFiboGenerator=makeFiboGenerator;
-exports.makeCycler=makeCycler;
-exports.compose=compose;*/
+exports.makeConstant=makeConstant;
+exports.makeCounterFromN=makeCounterFromN;
+exports.makeCounterFromZero=makeCounterFromZero;
+exports.makeDeltaTracker=makeDeltaTracker;
+exports.compose=compose;
+//exports.curry=curry;
+//exports.makeFiboGenerator=makeFiboGenerator;
+//exports.makeCycler=makeCycler;
