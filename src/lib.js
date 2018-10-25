44b082660297bbfd98750d511ca59724860d1069
Raja Pothanaboiena <44020191+rajapothana@users.noreply.github.com>
2018-10-25 13:02:40 +0530

    now lib.js file is working for makeCounterFromZero problem

--- src/lib.js
+++ src/lib.js
@@ -1,17 +1,34 @@
-const makeConstant = undefined;
-const makeCounterFromN = undefined;
-const makeCounterFromZero = undefined;
-const makeDeltaTracker = undefined;
+const makeConstant = function (source) {
+  return function (anything){
+    return source;
+  }
+}
+
+const makeCounterFromN = function (argv) {
+  let count = argv;
+  return function (){
+  return count++;
+  }
+}
+
+const makeCounterFromZero = function () {
+  let count = 0;
+  return function () {
+    return count++;
+  }
+}
+
+/*const makeDeltaTracker = undefined;
 const makeFiboGenerator = undefined;
 const makeCycler = undefined;
 const curry = undefined;
 const compose = undefined;
-
+*/
 exports.makeConstant=makeConstant;
-exports.makeCounterFromZero=makeCounterFromZero;
 exports.makeCounterFromN=makeCounterFromN;
-exports.makeDeltaTracker=makeDeltaTracker;
+exports.makeCounterFromZero=makeCounterFromZero;
+/*exports.makeDeltaTracker=makeDeltaTracker;
 exports.makeFiboGenerator=makeFiboGenerator;
 exports.makeCycler=makeCycler;
 exports.curry=curry;
-exports.compose=compose;
+exports.compose=compose;*/
